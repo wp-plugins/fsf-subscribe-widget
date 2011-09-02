3ed14fbeabfa6f2ba70709cdd46236b310a33eef
sethradio <sethradio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-02 00:47:41 +0000

    Committing first release.
    git-svn-id: https://plugins.svn.wordpress.org/fsf-subscribe-widget/trunk@432008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ FSF-Subscriber-Widget.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: FSF Subscribe Widget
+Plugin URI: http://sethradio.com/projects/wp-plugins
+Description: An FSF Mailing List subscribe widget for WordPress.
+Author: Seth Schroeder
+Version: 1.0
+Author URI: http://sethradio.com/
+*/
+###########################################################################
+#Copyright (C) 2011  Seth Schroeder                                       #
+#This program is free software: you can redistribute it and/or modify     #
+#it under the terms of the GNU General Public License as published by     #
+#the Free Software Foundation, either version 3 of the License, or        #
+#(at your option) any later version.                                      #
+#This program is distributed in the hope that it will be useful,          #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#GNU General Public License for more details.                             #
+#You should have received a copy of the GNU General Public License        #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
+###########################################################################
+
+ 
+function fsfsubscribe()
+{
+  echo "<center><p><h4>Support software freedom!</h4></p><p><i>Get the new monthly newsletter</i></p><form method=\"post\" action=\"https://crm.fsf.org/civicrm/profile/create&reset=1&gid=31\"><p><input type=\"text\" onfocus=\"this.value=''\" value=\"you@example.com\" size=\"15\" name=\"email-Primary\"></p><p><input type=\"submit\" value=\"Subscribe Me\" name=\"_qf_Edit_next\"></p></form></center>";
+}
+ 
+function widget_fsf($args) {
+  extract($args);
+  echo $before_widget;
+  echo $before_title;?>Subscribe to FSF<?php echo $after_title;
+  fsfsubscribe();
+  echo $after_widget;
+}
+ 
+function fsf_init()
+{
+  register_sidebar_widget(__('FSF Subscribe Widget'), 'widget_fsf');
+}
+add_action("plugins_loaded", "fsf_init");
+?>
---
+++ readme.txt
@@ -0,0 +1,21 @@
+===FSF Subscribe Widget ===
+Contributors: sethradio
+Tags: fsf,gnu,open source,subscribe,widget
+Requires at least: 2.8
+Tested up to: 3.2.1
+Stable tag: 1.0
+
+Allows visitors to your blog to subscribe to the FSF mailing list.
+
+== Description ==
+
+This plugin is a widget that you can put on your blog that allows users to subscribe to the FSF mailing list. More features will come in later versions. Remember, in the wise words of Matt Mullenweg, "1.0 is the lonliest number."
+
+== Screenshots ==
+
+1. What the widget looks like on my site.
+
+== Changelog ==
+
+= 1.0 =
+* First Release.
